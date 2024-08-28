b0888eb491fef4869ac12d095e7d84ded33e02a0
Shaikh Mohammed Umair <mohumair1901@gmail.com>
2024-08-28 21:56:27 +0530

    removed

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
       <Feature />
       {/* <FunFact /> */}
       {/* <FAQ /> */}
-      <Contact />
+      {/* <Contact /> */}
       {/* <Blog /> */}
     </main>
   );
