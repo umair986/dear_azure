f589b0077ac8cc6027462e1df789d5088ce98613
Shaikh Mohammed Umair <mohumair1901@gmail.com>
2024-08-28 22:00:03 +0530

    dark mode logo added in footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -39,7 +39,7 @@ const Footer = () => {
                   <Image
                     width={110}
                     height={80}
-                    src="/images/logo/logo-dark.svg"
+                    src="/images/logo/Logo-dark.png"
                     alt="Logo"
                     className="hidden dark:block"
                   />
